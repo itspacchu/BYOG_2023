b74d3a9ae046e52ea4adba51b65214af67eddb45
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-09-16 00:04:33 +0530

    Polish

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -2,30 +2,41 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const AIR_SPEED = 3.0
 const JUMP_VELOCITY = 4.5
+const ROT_SMOOTHING = 20.0
+const SPRINT_MULTIPLIER = 1.75
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _rotate_player(delta):
+	$PlayerRot.rotation.y = lerp_angle($PlayerRot.rotation.y,atan2(-velocity.z,velocity.x),delta*ROT_SMOOTHING)
 
-func _physics_process(delta):
+func _process(delta):
 	# Add the gravity.
+	var move_speed = SPEED
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		move_speed = lerp(move_speed,AIR_SPEED,40*delta)
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if(Input.is_action_pressed("sprint")):
+		move_speed *= SPRINT_MULTIPLIER
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
+		_rotate_player(delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed * 10 * delta)
+		velocity.z = move_toward(velocity.z, 0, move_speed * 10 * delta)
 
 	move_and_slide()
