13cfac92c8b38b4244cc18a1cc49d06a08f2de0e
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-09-16 01:21:53 +0530

    use Target based dashing

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -7,11 +7,18 @@ const JUMP_VELOCITY = 4.5
 const ROT_SMOOTHING = 20.0
 const SPRINT_MULTIPLIER = 1.75
 
+var can_dash:bool = true
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _rotate_player(delta):
 	$PlayerRot.rotation.y = lerp_angle($PlayerRot.rotation.y,atan2(-velocity.z,velocity.x),delta*ROT_SMOOTHING)
+
+
+func dash_handler(delta):
+	var tweeni = create_tween()
+	tweeni.tween_property(self,"global_position",%DashTarget.global_position, 0.1)
 
 func _process(delta):
 	# Add the gravity.
@@ -28,8 +35,14 @@ func _process(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if(Input.is_action_pressed("sprint")):
+	if(Input.is_action_just_pressed("sprint") and can_dash):
 		move_speed *= SPRINT_MULTIPLIER
+		can_dash = false
+		$Cooldowns/DashTimer.start(1)
+		dash_handler(delta)
+	$Control/Button.disabled = not can_dash
+	if(can_dash):
+		$Control/Button.text = str(ceil($Cooldowns/DashTimer.time_left))
 	
 	if direction:
 		velocity.x = direction.x * move_speed
@@ -40,3 +53,7 @@ func _process(delta):
 		velocity.z = move_toward(velocity.z, 0, move_speed * 10 * delta)
 
 	move_and_slide()
+
+
+func _on_dash_timer_timeout():
+	can_dash = true
