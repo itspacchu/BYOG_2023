4f88c1017069138f141fed81df25ca289ec265f6
Shahbaaz Khan <51862748+TheAwesomeShaz@users.noreply.github.com>
2023-09-16 10:46:11 +0530

    Changed the Jump a bit

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const AIR_SPEED = 3.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 14.0
 const ROT_SMOOTHING = 20.0
 const SPRINT_MULTIPLIER = 1.75
 
@@ -33,10 +33,9 @@ func dash_handler(delta):
 func _process(delta):
 	# Add the gravity.
 	var move_speed = SPEED
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if not is_on_floor(): 
+		velocity.y -= 3 * gravity * delta
 		move_speed = lerp(move_speed,AIR_SPEED,40*delta)
-		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
